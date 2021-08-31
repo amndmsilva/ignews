efc5b8382040fda0eca461e698fb70dfb1581f04
Amanda Santos <amndmsilva@gmail.com>
2021-08-30 22:42:46 -0300

    first commit

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,25 @@
+import { useSession } from 'next-auth/client'
+
+import styles from './styles.module.scss';
+
+interface SubscribeButtonProps {
+  priceId: string;
+}
+
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+
+  function handleSubscribe() {
+
+  }
+
+  return (
+    <button 
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
+      Subscribe now
+    </button>
+  )
+}
